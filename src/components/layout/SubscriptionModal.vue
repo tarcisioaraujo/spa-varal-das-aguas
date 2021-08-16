8dc07d39a3d3d2d675d30738381805178effdea4
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2021-08-16 01:19:28 -0300

    Melhora labels do formulário de cadastro e digita termos de uso

--- src/components/layout/SubscriptionModal.vue
+++ src/components/layout/SubscriptionModal.vue
@@ -21,37 +21,37 @@
                 <form>
                     <div class="form-row">
                         <div class="form-group col-md-6">
-                            <label for="inputName4">Nome do colaborador:</label>
+                            <label for="inputName4" class="font-weight-bold">Nome do colaborador:</label>
                             <input type="text" class="form-control" id="inputName4" placeholder="Insira seu nome" v-model="inscricao.nome">
                         </div>
                         <div class="form-group col-md-6">
-                            <label for="inputNamePhoto4">Nome da foto:</label>
+                            <label for="inputNamePhoto4" class="font-weight-bold">Nome da foto:</label>
                             <input type="text" class="form-control" id="inputNamePhoto4" placeholder="Insira o nome da foto" v-model="inscricao.nome_foto">
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-6">
-                            <label for="inputNameLacation4">Local da foto:</label>
+                            <label for="inputNameLacation4" class="font-weight-bold">Local da foto:</label>
                             <input type="text" class="form-control" id="inputNameLacation4" placeholder="Insira o local da foto" v-model="inscricao.local_foto">
                         </div>
                         <div class="form-group col-md-6">
-                            <label for="inputDate4">Data de registro:</label>
+                            <label for="inputDate4" class="font-weight-bold">Data de registro:</label>
                             <input type="text" class="form-control" id="inputDate4" placeholder="MM/DD/YYYY" v-model="inscricao.data_foto">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="photoFile1">Anexar foto</label>
+                        <label for="photoFile1" class="font-weight-bold">Anexar foto</label>
                         <input type="file" class="form-control-file" id="photoFile1" @change="processFile($event)">
                     </div>
                     <div class="form-group">
-                        <label for="terms">Termos</label>
-                        <textarea class="form-control" id="terms" rows="4" placeholder="" required>I. Onoaneoan oeaneonao na&#10;&#10;II. Onon nonon onnonon&#10;&#10;III. Ono nono nono nono non on nonon Onon&#10;&#10;IV. Onon onononono nonon o&#10;&#10;
+                        <label for="terms" class="font-weight-bold">Termos</label>
+                        <textarea class="form-control" id="terms" rows="7" placeholder="" required>I. Confirmo autoria da fotografia, que não constitui plágio em espécie alguma e não possui manipulação digital ou sobreposição;&#10;II. Declaro cessão de direito de uso e publicação da fotografia "Varal das Águas";&#10;III. Autorização para veiculação de imagem e nome do colaborador, nome artístico para a foto, local e data de registro da foto, bem como permissão para os organizadores a critério próprio, sem limite de tempo e sem a incidência de quaisquer ônus utilizarem ou divulgarem as imagens inscritas;
                         </textarea>
                     </div>                  
                     <div class="form-group">
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" v-model="inscricao.termos" required>
-                            <label class="form-check-label" for="invalidCheck2">
+                            <label class="form-check-label font-weight-bold" for="invalidCheck2">
                                 Eu li e concordo com os termos
                             </label>
                         </div>
@@ -100,15 +100,14 @@ export default {
       });
     },
     sendSubscribe() {      
-      // return axios.post('http://apidev.inema.ba.gov.br/participante', {
-      //   nome: this.inscricao.nome,
-      //   nome_foto: this.inscricao.nome_foto,
-      //   local_foto: this.inscricao.local_foto,
-      //   data_foto: this.inscricao.data_foto,
-      //   img_base64: this.inscricao.fileImage,
-      //   termos: this.inscricao.termos        
-      // });
-      return Promise.resolve('Success')
+      return axios.post('http://apidev.inema.ba.gov.br/participante', {
+        nome: this.inscricao.nome,
+        nome_foto: this.inscricao.nome_foto,
+        local_foto: this.inscricao.local_foto,
+        data_foto: this.inscricao.data_foto,
+        img_base64: this.inscricao.fileImage,
+        termos: this.inscricao.termos        
+      });      
     },
     async processSubscribe(){ 
       var vm = this 
@@ -127,9 +126,6 @@ export default {
         $('#success-alert').hide();
         return
       });
-
-      
-
     }
   }
 }
